715df4eb028f9eda310ecfa162b96d33f2f047de
MichelleVandes <m.vandeschrick@codeur.online>
2022-01-24 18:56:00 +0000

    carousel

--- 2022-01-24_Carousel/2022-01-24_carousel.js
+++ 2022-01-24_Carousel/2022-01-24_carousel.js
@@ -1,43 +1,22 @@
 function carousel_suivant () {
 
 
-  var myUL = document.getElementsByTagName("ul")[0];
-  var li = myUL.getElementsByTagName("li");
-  for (var i in li) {
-    alert(li);
-  }
-}
+  let myUL = document.getElementsByTagName("img");
+  let der_image;
+  // alert(myUL);
+   for(let i = 0; i<myUL.length; i++) {
+     console.log(der_image);
 
-var slidePosition = 1;
-SlideShow(slidePosition);
+     console.log(i);
+     if (myUL[i].className= "active") {
 
-// forward/Back controls
-function plusSlides(n) {
-  SlideShow((slidePosition += n));
-}
+       myUL[i].className = "inactive";
+        myUL[i+1].className = "active";
+       console.log(myUL[i].className);}
+       break;
+      
+      }
 
-//  images controls
-function currentSlide(n) {
-  SlideShow((slidePosition = n));
-}
-
-function SlideShow(n) {
-  var i;
-  var slides = document.getElementsByClassName("Containers");
-  var circles = document.getElementsByClassName("dots");
-  if (n > slides.length) {
-    slidePosition = 1;
-  }
-  if (n < 1) {
-    slidePosition = slides.length;
-  }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < circles.length; i++) {
-    circles[i].className = circles[i].className.replace(" enable", "");
-  }
-  slides[slidePosition - 1].style.display = "block";
-  circles[slidePosition - 1].className += " enable";
-} 
+       };
+   
 
