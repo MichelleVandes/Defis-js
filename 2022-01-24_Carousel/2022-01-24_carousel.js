77db69afb194fa0b873290ff29b4d3812089dc13
MichelleVandes <m.vandeschrick@codeur.online>
2022-01-25 13:35:21 +0000

    carousel fonctionne

--- 2022-01-24_Carousel/2022-01-24_carousel.js
+++ 2022-01-24_Carousel/2022-01-24_carousel.js
@@ -1,22 +1,51 @@
-function carousel_suivant () {
+let myUL, suivant, precedent, der_image; 
 
-
-  let myUL = document.getElementsByTagName("img");
-  let der_image;
+function carousel_suivant() {
   // alert(myUL);
-   for(let i = 0; i<myUL.length; i++) {
-     console.log(der_image);
+ myUL = document.getElementsByTagName("img");
+  suivant = document.getElementById("suivant");
+  precedent = document.getElementById("precedent");
 
-     console.log(i);
-     if (myUL[i].className= "active") {
+  for (let i = 0; i < myUL.length; i++) {
+   // console.log(i,myUL.length, suivant.className);
 
-       myUL[i].className = "inactive";
-        myUL[i+1].className = "active";
-       console.log(myUL[i].className);}
+    if ((myUL[i].className == "active")) {
+      myUL[i].className = "inactive";
+      myUL[i + 1].className = "active";
+      precedent.className = "est_visible";
+
+      console.log(i, myUL.length);
+      if (i+1== myUL.length -1) { 
+       // console.log("top1")
+        suivant.className = "est_invisible";}
        break;
-      
-      }
-
-       };
+    }
    
+  }
+}
 
+function carousel_precedent() {
+  myUL = document.getElementsByTagName("img");
+  suivant = document.getElementById("suivant");
+  precedent = document.getElementById("precedent");
+
+  for (let i = myUL.length-1; i > 0; i--) {
+    console.log(i, myUL[i]);
+
+    if (myUL[i].className == "active") {
+      myUL[i].className = "inactive";
+      myUL[i  -1].className = "active";
+      precedent.className = "est_visible";
+    console.log(i, myUL.length);
+    if (i - 2 < myUL.length ) {
+      console.log("top2", i);
+      suivant.className = "est_visible";
+    }
+    if (i === 1) {
+      console.log("top3", i);
+      precedent.className = "est_invisible";
+    }
+      break;
+    }
+  }
+}
