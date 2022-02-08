99013fde2bc097c490b979d37a588cebbbae37d1
MichelleVandes <m.vandeschrick@codeur.online>
2022-02-08 10:55:19 +0000

    fic json

--- 2022-02-03_Fichier_JSON/script.js
+++ 2022-02-03_Fichier_JSON/script.js
@@ -2,13 +2,12 @@ let mon_fic, mon_fic_propose, ma_donnee;
 
 
 // Fichier json par défaut, à l'initialisation de l'écran
- console.log(mon_fic);
-if (mon_fic == undefined) {
+ 
   mon_fic = "FR.json";
   console.log("top 1");
 
   remplir_zone(mon_fic);
-}
+
 
 // Changement de Langue
 function chgt() {
@@ -22,6 +21,7 @@ console.log(mon_fic);
 fetch(mon_fic).then(function (response) {
   response.json().then(function (data) {
     console.log(data.h1);
+
 
     ma_donnee = document.getElementById("titre_h1");
     ma_donnee.innerHTML = data.h1;
